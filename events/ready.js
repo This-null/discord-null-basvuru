86ba8aaeb352b34ee4aa2d70ea8e2e0d5b93b8d8
null <60463845+This-null@users.noreply.github.com>
2021-08-04 17:22:27 +0300

    Update ready.js

--- events/ready.js
+++ events/ready.js
@@ -7,8 +7,8 @@ var prefix = configirasyon.prefix;
 
 module.exports = client => {
   client.user.setPresence({ activity: { name: "null 💛 Shields" }, status: "idle" });
-  let botVoiceChannel = client.channels.cache.get(configirasyon.seskanalı);
-  if (botVoiceChannel) botVoiceChannel.join().catch(err => console.error("Ses kanalına giriş başarısız"));
+  let sex = client.channels.cache.get(configirasyon.seskanalı);
+  if (sex) sex.join().catch(err => console.error("Ses kanalına giriş başarısız"));
   console.log(`Aktifim Orospu evladı`);
 
 };
